29743901709c76deb85a82394113b97facb28269
Deneth Nadeeja <denethsaputhanthri@gmail.com>
2026-09-22 15:25:54 +0530

    initial setup

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <div className="flex justify-center items-center h-screen">
+      <h1 className="bg-blue-800 text-white p-4">Welcome to the Real Estate App</h1>
+    </div>
+    </>
+  )
+}
+
+export default App
